d50f65dea08d438a266ce94b13afe90b11ae674e
huajian.yu@wormpex.com <huajian.yu@wormpex.com>
2017-08-16 15:39:30 +0800

    begin

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'airbnb-base',
+    rules: {
+        'comma-dangle': ['error', 'never'],
+        'func-names': 'off',
+        'max-len': ['error', 200],
+        'no-plusplus': 'off',
+        'no-param-reassign': 'off',
+        'one-var': 'off',
+        'one-var-declaration-per-line': 'off'
+  }
+}
